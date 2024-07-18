0094c464cdf6c453dfbab2ba73e8e24798202dec
Daniel Steman <daniel-steman@live.nl>
2024-07-18 19:31:06 +0200

    create saltedge client

---
+++ app/utils/saltedge.py
@@ -0,0 +1,32 @@
+import os
+import httpx
+
+
+PROVIDERS_URL = "https://www.saltedge.com/api/v5/providers"
+
+
+class SaltEdgeConfig:
+    def __init__(self):
+        self.app_id = os.environ.get("app_id")
+        self.secret = os.environ.get("secret")
+        if self.app_id is None:
+            raise ValueError("Saltedge APP_ID is not set")
+        if self.secret is None:
+            raise ValueError("Saltedge SECRET is not set")
+
+
+class SaltEdgeClient(httpx.Client):
+    def __init__(self, config: SaltEdgeConfig):
+        super().__init__(
+            headers={
+                "Accept": "application/json",
+                "Content-type": "application/json",
+                "App-id": config.app_id,
+                "Secret": config.secret,
+            }
+        )
+
+    def list_providers(self):
+        response = self.get(PROVIDERS_URL)
+        response.raise_for_status()
+        return response.json()
