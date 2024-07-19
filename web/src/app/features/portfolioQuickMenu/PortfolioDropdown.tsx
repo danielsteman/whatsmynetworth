0c0f34f67c57682247eef88a7d9ba170e300e9dc
Daniel Steman <daniel-steman@live.nl>
2024-07-19 09:49:03 +0200

    add new account button

--- web/src/app/features/portfolioQuickMenu/PortfolioDropdown.tsx
+++ web/src/app/features/portfolioQuickMenu/PortfolioDropdown.tsx
@@ -7,6 +7,7 @@ import {
 } from "./portfolioQuickMenuSlice";
 import { RootState } from "@/lib/store";
 import { useDispatch, useSelector } from "react-redux";
+import { FiPlus } from "react-icons/fi";
 
 interface PortfolioDropdownProps {
   dropDown: PortfolioQuickMenuDropdowns;
@@ -19,17 +20,28 @@ const PortfolioDropdown: React.FC<PortfolioDropdownProps> = ({ dropDown }) => {
   );
 
   return (
-    <div
-      className="flex flex-row gap-4 items-center py-4 px-2 rounded-xl cursor-pointer hover:bg-neutral-100"
-      onClick={() => dispatch(toggleDropdown(dropDown))}
-    >
-      {droppedDownItems[dropDown] === false ? (
-        <IoIosArrowForward className="text-neutral-500" size={20} />
-      ) : (
-        <IoIosArrowDown className="text-neutral-500" size={20} />
+    <div className="flex flex-col">
+      <div
+        className="flex flex-row gap-4 py-4 px-2 items-center rounded-xl cursor-pointer hover:bg-neutral-100"
+        onClick={() => dispatch(toggleDropdown(dropDown))}
+      >
+        {droppedDownItems[dropDown] === false ? (
+          <IoIosArrowForward className="text-neutral-500" size={20} />
+        ) : (
+          <IoIosArrowDown className="text-neutral-500" size={20} />
+        )}
+        <div className="text-sm font-medium ">{dropDown}</div>
+      </div>
+      {droppedDownItems[dropDown] && (
+        <div className="flex flex-col py-4 px-2 items-center rounded-xl cursor-pointer hover:bg-neutral-100">
+          <div className="flex flex-row gap-4">
+            <FiPlus size={20} className="text-neutral-500" />
+            <div className="text-sm font-medium text-neutral-500">
+              New {dropDown.toLowerCase()}
+            </div>
+          </div>
+        </div>
       )}
-
-      <div className="text-sm font-medium ">{dropDown}</div>
     </div>
   );
 };
