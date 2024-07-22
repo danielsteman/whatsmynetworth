c80774f5d9af9d6fd0fa300cc5d81cd38f59775f
Daniel Steman <daniel-steman@live.nl>
2024-07-22 12:30:46 +0200

    restructure

--- app/tests/test_saltedge.py
+++ app/tests/test_saltedge.py
@@ -1,8 +1,8 @@
-from app.utils.saltedge import SaltEdgeClient
+from app.utils.saltedge.client import SaltEdgeClient
 
 
 def test_list_providers():
     client = SaltEdgeClient()
-    providers = client.list_providers()
+    providers = client.list_providers(country_code=None)
     print(providers)
     assert len(providers) > 0
