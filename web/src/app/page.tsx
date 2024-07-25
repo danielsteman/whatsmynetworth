b5adb6e4733969a0d0d79fe16d3d416d016de0d8
Daniel Steman <daniel-steman@live.nl>
2024-07-25 22:12:50 +0200

    add login page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,15 @@
+import Logo from "./features/navigation/Logo";
+
 export default function Home() {
-  return <div>hoi</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-center">
+        <div className="flex justify-center mb-4">
+          <Logo />
+        </div>
+        <h1 className="text-3xl font-semibold">Sign in to your account</h1>
+        <p className="mt-4 text-md">Or create an account</p>
+      </div>
+    </div>
+  );
 }
