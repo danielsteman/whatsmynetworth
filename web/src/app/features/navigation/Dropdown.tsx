593b123f283ef40d650a8f9d2744ee4ff0c7123d
Daniel Steman <daniel-steman@live.nl>
2024-08-10 16:14:03 +0200

    close dropdown if user clicks outside of menu

--- web/src/app/features/navigation/Dropdown.tsx
+++ web/src/app/features/navigation/Dropdown.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { ButtonHTMLAttributes, ReactNode, useState } from "react";
+import {
+  ButtonHTMLAttributes,
+  ReactNode,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { FiLogOut, FiSettings } from "react-icons/fi";
 
 interface DropdownProps {
@@ -30,8 +36,26 @@ const DropdownItem: React.FC<DropdownItemProps> = ({
 
 const Dropdown: React.FC<DropdownProps> = ({ children }) => {
   const [toggle, setToggle] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setToggle(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
   return (
-    <div className="flex flex-col relative">
+    <div className="flex flex-col relative" ref={dropdownRef}>
       <div onClick={() => setToggle(!toggle)}>{children}</div>
       {toggle && (
         <div className="flex flex-col gap-2 absolute top-10 w-fit bg-neutral-200 p-2 rounded-md min-w-32">
