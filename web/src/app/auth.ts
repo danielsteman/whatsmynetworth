0bd164875f0fa9870b38f7181c6b0ecb5c782ec3
Daniel Steman <daniel-steman@live.nl>
2024-08-14 20:56:52 +0200

    init prisma client

--- web/src/app/auth.ts
+++ web/src/app/auth.ts
@@ -1,5 +1,9 @@
 import GoogleProvider from "next-auth/providers/google";
 import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const authOptions: NextAuthOptions = {
   providers: [
@@ -8,6 +12,7 @@ const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  adapter: PrismaAdapter(prisma),
 };
 
 export default authOptions;
