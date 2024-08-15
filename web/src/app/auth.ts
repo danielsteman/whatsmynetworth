abce1387feb1f420a0061aa110c3d81150f93fa5
Daniel Steman <daniel-steman@live.nl>
2024-08-15 21:15:26 +0200

    added sign in callback to create saltedge customer

--- web/src/app/auth.ts
+++ web/src/app/auth.ts
@@ -15,14 +15,30 @@ const authOptions: NextAuthOptions = {
   ],
   adapter: <Adapter>PrismaAdapter(prisma),
   callbacks: {
-    async signIn({
-      user,
-      account,
-      profile,
-      email,
-      credentials,
-    }): Promise<boolean> {
-      return true;
+    async signIn({ user }): Promise<boolean> {
+      try {
+        const customerId = user.id;
+        const response = await fetch(
+          `${process.env.BACKEND_BASE_URL}/api/customers/create`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ id: customerId }),
+          }
+        );
+
+        if (!response.ok) {
+          console.error("Failed to create customer", await response.text());
+          return false;
+        }
+
+        return true;
+      } catch (error) {
+        console.error("Error in sign-in callback:", error);
+        return false;
+      }
     },
   },
 };
