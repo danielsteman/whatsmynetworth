d83f411de7f290711ee4bbf76b86c985ac5066c1
Daniel Steman <daniel-steman@live.nl>
2024-08-15 21:00:47 +0200

    refactor tests

---
+++ app/tests/test_connection.py
@@ -0,0 +1,9 @@
+def test_create_connection(client):
+    mock_id = "123"
+    try:
+        customer = client.create_customer(mock_id)
+        customer_id = customer.id
+        connect_session = client.create_connect_session(customer_id)
+        assert connect_session
+    finally:
+        client.delete_customer(customer_id)
--- app/tests/test_saltedge.py
+++ app/tests/test_saltedge.py
@@ -10,14 +10,3 @@ def test_validate_all_providers_in_list(client):
     providers = client.list_providers(country_code=None)
     for p in providers:
         assert isinstance(p, Provider)
-
-
-def test_create_connection(client):
-    mock_id = "123"
-    try:
-        customer = client.create_customer(mock_id)
-        customer_id = customer.id
-        connect_session = client.create_connect_session(customer_id)
-        assert connect_session
-    finally:
-        client.delete_customer(customer_id)
