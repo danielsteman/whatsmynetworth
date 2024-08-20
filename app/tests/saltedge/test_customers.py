fdf2c3c140b69343927c4dd918a1c91abadedb8b
Daniel Steman <daniel-steman@live.nl>
2024-08-20 22:26:00 +0200

    add unit test

--- app/tests/saltedge/test_customers.py
+++ app/tests/saltedge/test_customers.py
@@ -1,3 +1,8 @@
+import pytest
+
+from app.utils.saltedge.exceptions import CustomerAlreadyExists
+
+
 def test_create_get_delete_customer(client):
     mock_id = "123"
     customer = client.create_customer(mock_id)
@@ -8,3 +13,11 @@ def test_create_get_delete_customer(client):
     assert customer_info, "expected customer info"
     deleted_customer = client.delete_customer(customer_id)
     assert deleted_customer, "expected deleted customer info"
+
+
+def test_customer_already_exists(client):
+    mock_id = "321"
+    customer = client.create_customer(mock_id)
+    with pytest.raises(CustomerAlreadyExists):
+        client.create_customer(mock_id)
+    client.delete_customer(customer.id)
