e6b63b3c1bfb3ab07158d9d96d64681ea528b80d
Daniel Steman <daniel-steman@live.nl>
2024-08-21 16:46:18 +0200

    close new account menu when clicking outside of the menu

--- web/src/app/features/newAccount/NewAccountOverlay.tsx
+++ web/src/app/features/newAccount/NewAccountOverlay.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { AppDispatch } from "@/lib/store";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useRef } from "react";
 import { IoCloseCircle } from "react-icons/io5";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "./newAccountMenuSlice";
@@ -12,9 +12,27 @@ interface NewAccountOverlayProps {
 
 const NewAccountOverlay: React.FC<NewAccountOverlayProps> = ({ children }) => {
   const dispatch = useDispatch<AppDispatch>();
+  const overlayRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        overlayRef.current &&
+        !overlayRef.current.contains(event.target as Node)
+      ) {
+        dispatch(toggleMenu());
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [overlayRef]);
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white shadow-lg relative rounded-xl">
+      <div className="bg-white shadow-lg relative rounded-xl" ref={overlayRef}>
         {children}
         <button
           onClick={() => dispatch(toggleMenu())}
