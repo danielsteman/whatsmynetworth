fbf6cfe038920c47582f920c97e85e4289fa0749
Daniel Steman <daniel-steman@live.nl>
2024-08-21 22:09:16 +0200

    make new account menu dynamic

--- web/src/app/features/newAccount/NewAccountMenu.tsx
+++ web/src/app/features/newAccount/NewAccountMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { IoIosLink, IoMdArrowBack } from "react-icons/io";
 import { RiBankFill } from "react-icons/ri";
 
 type MenuSteps = 0 | 1;
@@ -14,7 +15,10 @@ const FirstStepMenu: React.FC<StepMenuProps> = ({ setMenuStep }) => (
     <h2 className="text-neutral-500 border-b-2 py-4 w-full">
       What would you like to add?
     </h2>
-    <button className="font-medium w-full hover:bg-neutral-200 rounded-xl p-3">
+    <button
+      className="font-medium w-full hover:bg-neutral-200 rounded-xl p-3"
+      onClick={() => setMenuStep(1)}
+    >
       <div className="flex flex-row gap-4">
         <RiBankFill size={24} />
         <div>Payment account</div>
@@ -25,12 +29,18 @@ const FirstStepMenu: React.FC<StepMenuProps> = ({ setMenuStep }) => (
 
 const SecondStepMenu: React.FC<StepMenuProps> = ({ setMenuStep }) => (
   <>
-    <h2 className="text-neutral-500 border-b-2 py-4 w-full">
-      How would you like to add it?
-    </h2>
+    <div className="flex flex-row gap-4 border-b-2 w-full items-center text-neutral-500">
+      <button
+        onClick={() => setMenuStep(0)}
+        className="p-1 rounded-lg bg-neutral-200 hover:bg-neutral-300"
+      >
+        <IoMdArrowBack size={24} />
+      </button>
+      <h2 className=" py-4 w-full">How would you like to add it?</h2>
+    </div>
     <button className="font-medium w-full hover:bg-neutral-200 rounded-xl p-3">
       <div className="flex flex-row gap-4">
-        <RiBankFill size={24} />
+        <IoIosLink size={24} />
         <div>Link bank account safely through open banking</div>
       </div>
     </button>
