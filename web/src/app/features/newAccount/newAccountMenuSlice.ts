9fb70cff3c53c8e5c73b35df0e6e63d7fd231f10
Daniel Steman <daniel-steman@live.nl>
2024-08-21 22:24:42 +0200

    add reducer

--- web/src/app/features/newAccount/newAccountMenuSlice.ts
+++ web/src/app/features/newAccount/newAccountMenuSlice.ts
@@ -1,11 +1,15 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+type MenuStep = 0 | 1;
+
 interface NewAccountMenuState {
   open: boolean;
+  step: MenuStep;
 }
 
 const initialState: NewAccountMenuState = {
   open: false,
+  step: 0,
 };
 
 const newAccountMenuSlice = createSlice({
@@ -15,9 +19,12 @@ const newAccountMenuSlice = createSlice({
     toggleMenu: (state) => {
       state.open = !state.open;
     },
+    setStep: (state, action: PayloadAction<MenuStep>) => {
+      state.step = action.payload;
+    },
   },
 });
 
-export const { toggleMenu } = newAccountMenuSlice.actions;
+export const { toggleMenu, setStep } = newAccountMenuSlice.actions;
 
 export default newAccountMenuSlice.reducer;
