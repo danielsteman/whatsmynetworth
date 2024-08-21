db15aaf996f8d307c8022a8e18595864e821f8de
Daniel Steman <daniel-steman@live.nl>
2024-08-21 16:25:38 +0200

    refactor

--- web/src/app/features/newAccount/NewAccountOverlay.tsx
+++ web/src/app/features/newAccount/NewAccountOverlay.tsx
@@ -6,20 +6,16 @@ import { IoCloseCircle } from "react-icons/io5";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "./newAccountMenuSlice";
 
-interface OverlayProps {
+interface NewAccountOverlayProps {
   children: ReactNode;
 }
 
-const Overlay: React.FC<OverlayProps> = ({ children }) => {
+const NewAccountOverlay: React.FC<NewAccountOverlayProps> = ({ children }) => {
   const dispatch = useDispatch<AppDispatch>();
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-8 rounded-lg shadow-lg relative">
         {children}
-        <h2 className="text-2xl font-bold">Centered Overlay</h2>
-        <p className="mt-4">
-          This overlay is centered horizontally and vertically.
-        </p>
         <button
           onClick={() => dispatch(toggleMenu())}
           className="absolute top-2 right-2 text-black font-bold"
@@ -31,4 +27,4 @@ const Overlay: React.FC<OverlayProps> = ({ children }) => {
   );
 };
 
-export default Overlay;
+export default NewAccountOverlay;
