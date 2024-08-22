7469ea4f4c4e52fe2e878ef4615ca9e37471b905
Daniel Steman <daniel-steman@live.nl>
2024-08-22 15:32:51 +0200

    add customer table

---
+++ app/alembic/versions/bd54f3e3021d_create_customer_table.py
@@ -0,0 +1,37 @@
+"""create customer table
+
+Revision ID: bd54f3e3021d
+Revises:
+Create Date: 2024-08-22 15:20:35.636240
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "bd54f3e3021d"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+# class Customer(Base):
+#     __tablename__ = "customers"
+
+
+def upgrade() -> None:
+    op.create_table(
+        "customers",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("identifier", sa.String, unique=True, index=True),
+        sa.Column("secret", sa.String),
+        sa.Column("updated_at", sa.String),
+        sa.Column("created_at", sa.String),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("customers")
