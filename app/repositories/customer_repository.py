d1757b6f3e033b1d4e8dc81d21b277770ae957b4
Daniel Steman <daniel-steman@live.nl>
2024-08-22 21:53:10 +0200

    remove comments

--- app/repositories/customer_repository.py
+++ app/repositories/customer_repository.py
@@ -4,18 +4,12 @@ from app import models
 
 
 def get_customer_by_identifier(db: Session, identifier: str) -> models.Customer | None:
-    """
-    Fetch a customer from the database by identifier.
-    """
     return db.query(models.Customer).filter_by(identifier=identifier).first()
 
 
 def create_customer_in_db(
     db: Session, id: str, identifier: str, secret: str, created_at: str, updated_at: str
 ) -> models.Customer:
-    """
-    Create a new customer in the database.
-    """
     db_customer = models.Customer(
         id=id,
         identifier=identifier,
