7b77c2a345916fd8cd9f018c569fb6dceaf3bed6
Daniel Steman <daniel-steman@live.nl>
2024-08-22 15:58:46 +0200

    fix imports

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from .connection import Connection
+from .customer import CreateCustomer, Customer
+from .provider import Provider
+
+__all__ = ["Customer", "CreateCustomer", "Connection", "Provider"]
