7f6e1992b6fc2f108ccf818ddddcb19abfb646ae
Daniel Steman <daniel-steman@live.nl>
2024-08-28 13:03:58 +0200

    settings page wip

--- web/src/app/settings/page.tsx
+++ web/src/app/settings/page.tsx
@@ -4,8 +4,14 @@ import { MdAccountCircle } from "react-icons/md";
 
 const Settings = () => {
   return (
-    <div className="flex flex-row p-4">
+    <div className="flex flex-row p-8 gap-24">
       <div className="flex flex-col">
+        <div className="w-full relative flex gap-2 items-center pb-2">
+          <h3 className="font-medium text-neutral-500 text-xs uppercase">
+            general
+          </h3>
+          <div className="h-px bg-neutral-200 w-full"></div>
+        </div>
         <MenuItem
           label="Account"
           icon={MdAccountCircle}
@@ -19,7 +25,9 @@ const Settings = () => {
           href="connections"
         />
       </div>
-      <div></div>
+      <div>
+        <h2>Account</h2>
+      </div>
     </div>
   );
 };
