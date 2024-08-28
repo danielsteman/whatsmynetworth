7365c830dfea02be4ee231d72d5771a9fd9763db
Daniel Steman <daniel-steman@live.nl>
2024-08-28 16:44:34 +0200

    add back button

--- web/src/app/features/settings/SettingsSideMenu.tsx
+++ web/src/app/features/settings/SettingsSideMenu.tsx
@@ -2,7 +2,7 @@
 
 import MenuItem from "@/app/components/MenuItem";
 import { IoIosLink } from "react-icons/io";
-import { MdAccountCircle } from "react-icons/md";
+import { MdAccountCircle, MdChevronLeft } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { select, SettingsMenuItem } from "./settingsMenuSlice";
 import { RootState } from "@/lib/store";
@@ -20,7 +20,14 @@ const SettingsSideMenu = () => {
   };
   return (
     <div className="flex flex-col">
-      <div className="w-full relative flex gap-2 items-center pb-2">
+      <a
+        href="/dashboard"
+        className="flex flex-row gap-1 w-fit items-center pr-2 py-1 mb-4 cursor-pointer rounded-lg hover:bg-neutral-200"
+      >
+        <MdChevronLeft size={24} className="text-neutral-500" />
+        <span className="font-medium text-sm">Back</span>
+      </a>
+      <div className="w-full relative flex gap-2 items-center py-2">
         <h3 className="font-medium text-neutral-500 text-xs uppercase">
           general
         </h3>
