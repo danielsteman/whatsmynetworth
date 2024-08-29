6396bdcc4fe78ed0d09a59a11057e4b6b2f32b81
Daniel Steman <daniel-steman@live.nl>
2024-08-29 22:29:46 +0200

    get connections in frontend

--- web/src/app/settings/page.tsx
+++ web/src/app/settings/page.tsx
@@ -3,14 +3,26 @@ import { redirect } from "next/navigation";
 import authOptions from "../auth";
 import SettingsSideMenu from "../features/settings/SettingsSideMenu";
 
+const getConnections = async (customerIdentifier: string) => {
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/api/connections`;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ identifier: customerIdentifier }),
+  });
+  const connections = await response.json();
+  return connections;
+};
+
 const Settings = async () => {
   const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/login");
   }
-  const url = `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/connections`;
-  // fetch()
-  console.log(session.user.id);
+  const connections = await getConnections(session.user.id);
+  console.log(connections);
   return (
     <div className="flex flex-row p-8 gap-12">
       <SettingsSideMenu />
