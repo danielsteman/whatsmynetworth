b4ee9a3f7747c3d079d45d942b5ae187289a88db
Daniel Steman <daniel-steman@live.nl>
2024-08-29 19:19:02 +0200

    placeholder frontend list connections

--- web/src/app/settings/page.tsx
+++ web/src/app/settings/page.tsx
@@ -1,6 +1,16 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import authOptions from "../auth";
 import SettingsSideMenu from "../features/settings/SettingsSideMenu";
 
-const Settings = () => {
+const Settings = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/login");
+  }
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/connections`;
+  // fetch()
+  console.log(session.user.id);
   return (
     <div className="flex flex-row p-8 gap-12">
       <SettingsSideMenu />
