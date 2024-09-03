5c564f50e54c4b901c6db2b3ac1e341a30172f75
Daniel Steman <daniel-steman@live.nl>
2024-09-03 15:18:31 +0200

    placeholder get transactions

--- app/repositories/transaction_repository.py
+++ app/repositories/transaction_repository.py
@@ -1,10 +1,9 @@
 from sqlalchemy.orm import Session
 
-# from app.repositories import connection_repository
 from app.utils.saltedge.client import SaltEdgeClient
 
 
-def get_transactions_by_customer_identifier(
+async def get_transactions(
     db: Session, client: SaltEdgeClient, identifier: str
 ) -> None:
     # connection = connection_repository.get_active_connection(db, client, identifier)
