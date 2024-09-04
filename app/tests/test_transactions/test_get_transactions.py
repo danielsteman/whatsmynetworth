b3c7a438f2333ea494babc055460594e5c58b7cb
Daniel Steman <daniel-steman@live.nl>
2024-09-04 21:43:13 +0200

    add test list transactions

--- app/tests/test_transactions/test_get_transactions.py
+++ app/tests/test_transactions/test_get_transactions.py
@@ -1,2 +1,11 @@
+def test_list_transactions(
+    client, consented_customer_id, consented_customer_account_id
+):
+    transactions = client.list_transactions(
+        consented_customer_account_id, consented_customer_id
+    )
+    assert transactions
+
+
 def test_get_transactions_by_customer_identifier() -> None:
     pass
