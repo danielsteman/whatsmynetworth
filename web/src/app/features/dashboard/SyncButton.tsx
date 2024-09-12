d9b9264307c849ff1c57aa11887e47cec463e4ed
Daniel Steman <daniel-steman@live.nl>
2024-09-12 22:22:51 +0200

    button icon animation

--- web/src/app/features/dashboard/SyncButton.tsx
+++ web/src/app/features/dashboard/SyncButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { FaSyncAlt } from "react-icons/fa";
 
 interface SyncButtonProps {
@@ -7,13 +8,24 @@ interface SyncButtonProps {
 }
 
 const SyncButton: React.FC<SyncButtonProps> = ({ connectionId }) => {
+  const [syncing, setSyncing] = useState<boolean>(false);
+
   const syncTransactions = (connectionId: string) => {
+    setSyncing(!syncing);
     console.log(`Syncing... using ${connectionId}`);
   };
+
+  const syncIconStyle = syncing ? "animate-spin" : "";
+
+  const baseButtonStyle =
+    "flex flex-row gap-2 items-center p-2 rounded-md hover:bg-neutral-200";
+  const additionalButtonStyle = syncing ? "bg-neutral-100" : "";
+  const buttonStyle = `${baseButtonStyle} ${additionalButtonStyle}`;
+
   return (
     <button onClick={() => syncTransactions(connectionId)}>
-      <div className="flex flex-row gap-2 items-center p-2 rounded-md hover:bg-neutral-200">
-        <FaSyncAlt />
+      <div className={buttonStyle}>
+        <FaSyncAlt className={syncIconStyle} />
         <span>Sync transactions</span>
       </div>
     </button>
